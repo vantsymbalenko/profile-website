f321b07ffa4ef7f1882be76ed2b85db371888b18
Ivan Tsymbalenko <ivanzymbalenko1@gmail.com>
2023-02-02 22:27:20 +0000

    feature/1 add card component

--- src/components/library/components/Card/Card.stories.tsx
+++ src/components/library/components/Card/Card.stories.tsx
@@ -1,10 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
 import { Card } from '.';
 
 export default {
-  title: 'Card',
-  component: Card,
-};
+	title: 'Card',
+	component: Card,
+} as ComponentMeta<typeof Card>;
 
-const Template = () => <Card />;
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
 export const Default = Template.bind({});
+
+Default.args = {
+	children:
+		'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis placeat tempora dolores quisquam doloremque quibusdam quos sint modi vero possimus quia, nemo, blanditiis harum magni eligendi. Neque vitae maxime explicabo.',
+};
--- src/components/library/config/theme/defaultTheme/index.ts
+++ src/components/library/config/theme/defaultTheme/index.ts
@@ -1,4 +1,25 @@
-export const defaultTheme = {
+import { DefaultTheme } from 'styled-components';
+
+const COLOR_TOKENS = {
+	blue50: '#007ced',
+	white0: '#ffffff',
+	gray50: '#333333',
+};
+
+const SHADOWS_TOKENS = {
+	black20: '0 0 40px -10px rgb(0 0 0 / 20%)',
+};
+
+export const defaultTheme: DefaultTheme = {
 	colors: {
+		primary: COLOR_TOKENS.gray50,
+		secondary: COLOR_TOKENS.white0,
+	},
+	background: {
+		primary: COLOR_TOKENS.blue50,
+		secondary: COLOR_TOKENS.white0,
+	},
+	shadows: {
+		primary: SHADOWS_TOKENS.black20,
 	},
 };
